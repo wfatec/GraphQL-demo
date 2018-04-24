cd2cb58f960f9e14db72b5f858253b9b8cca4d64
DELTA\Chao.BJ.Wang <chao125010@163.com>
2018-04-24 18:10:48 +0800

    add API for save data

--- mongodb/index.js
+++ mongodb/index.js
@@ -6,15 +6,16 @@ require('./schema/student')
 
 export const database = () => {
     mongoose.set('debug',true);
-    mongoose.connect(config.dbPath);
+    // mongoose.connect(config.dbPath);
+    mongoose.connect(config.dbPath, { useMongoClient: true });
 
     mongoose.connection.on('desconnected', () => {
-        mongoose.connect(config.dbPath);
+        mongoose.connect(config.dbPath, { useMongoClient: true });
     })
     mongoose.connection.on('error', err => {
         console.error(err);
     });
-    mongosoe.connection.on('open', async () => {
+    mongoose.connection.on('openUri', async () => {
         console.log('Connected to MongoDB', config.dbPath);
     })
 }
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import KoaStatic from 'koa-static'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 
+import { database } from './mongodb'
+import { saveInfo, fetchInfo } from './controllers/info'
+import { saveStudent, fetchStudent, fetchStudentDetail } from './controllers/student'
+
+database()
+
 const app = new Koa();
 const router = new Router();
 
@@ -12,6 +18,10 @@ app.use(KoaStatic(__dirname + '/public'));
 router.get('/test', (ctx, next) => {
     ctx.body = "test page";
 })
+
+router.post('/saveStudent', saveStudent)
+router.get('./student', fetchStudent)
+router.get('/studentDetail', fetchStudentDetail)
 
 app
     .use(router.routes())
